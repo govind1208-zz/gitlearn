ea005bdb0a27ea9048cb023838f1a50ef8a7af85
unknown <govindubuntu1208@gmail.com>
2021-07-24 21:49:30 +0530

    updates1

--- abc.js
+++ abc.js
@@ -1,9 +1,4 @@
 console.log('hello');
 console.log('hi');
 console.log('later on');
-<<<<<<< HEAD
 console.log('and on');
-=======
-
-console.log('and on');
->>>>>>> 6b678ccd743ab332ac2537689c937c57ffc19f3e
